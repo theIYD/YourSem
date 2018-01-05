d94cd3010fe490df48bb696d7d26c29fe4d2f114
Idrees <dargahidrees@gmail.com>
2018-01-05 20:53:02 +0530

    feature: Create login & register. Login to be made functional.

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+let userModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+mongoose.model('UserModel', userModel); 
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,79 @@
 //Bring the modules
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+
+//Bring in User Model
+require('../models/User');
+const User = mongoose.model('UserModel');
 
 //User Login router
 router.get('/login', (req, res) => {
-    res.send('login');
+    res.render('users/login');
 });
 
 //User Register Route
 router.get('/register', (req, res) => {
-    res.send('register');
+    res.render('users/register');
+});
+
+//Register Form POST
+router.post('/register', (req, res) => {
+    let errors = [];
+    //Check passwords match 
+    if (req.body.password !== req.body.password2) {
+        errors.push({
+            text: 'Passwords do not match'
+        });
+    }
+    //Check length of password
+    if (req.body.password.length < 4) {
+        errors.push({
+            text: 'Password must be at least of 4 characters'
+        });
+    }
+    //If an error exists
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+    } else {
+        User.findOne({
+                email: req.body.email
+            })
+            .then(user => {
+                if (user) {
+                    req.flash('errorMsg', `${user.email} already registered`);
+                    res.redirect('/users/register');
+                } else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    });
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw new Error(err);
+                            newUser.password = hash;
+                            newUser.save()
+                                .then((user) => {
+                                    req.flash('successMsg', 'You are now registered & can log in');
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    return;
+                                });
+                        });
+                    });
+                }
+            });
+    }
 });
 
 module.exports = router;
